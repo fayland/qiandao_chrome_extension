6f94a5e570f9083919eb75c00d648bafbf79c3a2
Fayland Lam <fayland@gmail.com>
2014-05-12 15:08:31 +0800

    tieba qiandao

---
+++ js/qiandao/tieba.js
@@ -0,0 +1,25 @@
+!function(win, doc, $){
+    $(doc).ready(function() {
+        var html = doc.documentElement.innerHTML;
+        var m1 = html.match(/tbs[\'\"]?:\s*[\'\"](\w+)[\'\"]/); // tbs:'zzzz'
+        var m2 = html.match(/fname=[\'\"]([^\'\"]+)[\'\"]/); // fname="Zzz"
+        if (m1 && m2) {
+            var tbs = m1[1];
+            var kw  = m2[1];
+            var status_key = "tieba_" + encodeURIComponent(kw) + "_status";
+            chrome.runtime.sendMessage({method: "getLocalStorage", key: status_key}, function(res) {
+                var status = res.data;
+                var today = (new Date()).toDateString();
+                if (! (status && today === status)) {
+                    // need cookie
+                    chrome.runtime.sendMessage({method: "tieba_qiandao", "tbs": tbs, "kw": kw}, function(res) {});
+                } else {
+                    console.log("[qiandao][tieba][" + kw + "] already signIn today.");
+                }
+            });
+        }
+
+
+
+    });
+}(window, document, Zepto);
--- src/bg/background.js
+++ src/bg/background.js
@@ -32,6 +32,7 @@
                 for (var i in cookies) {
                     if (cookies[i].name == 'login_sid_t') {
                         is_logined = 1;
+                        break;
                     }
                 }
                 console.log('is_logined:' + is_logined);
@@ -58,29 +59,32 @@
                 }
             });
         } else if (request.method == 'tieba_qiandao') {
-            console.log('tieba qiandao');
             chrome.cookies.getAll({domain:"tieba.baidu.com"}, function (cookies){
                 var is_logined = 0;
                 for (var i in cookies) {
-                    if (cookies[i].name == 'BAIDUID') {
+                    if (cookies[i].name == 'BAIDUID' || cookies[i].name == 'TIEBAUID') {
                         is_logined = 1;
+                        break;
                     }
                 }
-                console.log('is_logined:' + is_logined);
-                console.log(request);
-                is_logined = 0;
                 if (is_logined) {
                     var url = 'http://tieba.baidu.com/sign/add';
                     $.ajax({
                         type     : "POST",
                         url      : url,
+                        data     : {
+                            ie   : 'utf-8',
+                            kw   : request.kw,
+                            tbs  : request.tbs
+                        },
                         dataType : 'json',
                         success  : function(data) {
-                            console.log(data);
+                            // alert(JSON.stringify(data));
                             // {"no":0,"error":"","data":{"errno":0,"errmsg":"success","sign_version":2,"is_block":0,"finfo":{"forum_info":{"forum_id":8021166,"forum_name":"\u767e\u5ea6\u5f71\u68d2"},"current_rank_info":{"sign_count":1101}},"uinfo":{"user_id":889003992,"is_sign_in":1,"user_sign_rank":1101,"sign_time":1399875492,"cont_sign_num":1,"total_sign_num":2,"cout_total_sing_num":2,"hun_sign_num":1,"total_resign_num":0,"is_org_name":0}}}
-                            if (data.data.errno == 0) {
+                            // {"no":1101,"error":"亲，你之前已经签过了","data":""}
+                            if (data.no == 0 || data.no == 1101) {
                                 var today = (new Date()).toDateString();
-                                localStorage.setItem('tieba_status', today);
+                                localStorage.setItem("tieba_" + encodeURIComponent(request.kw) + "_status", today);
                             }
                         },
                         error: function(err) {
