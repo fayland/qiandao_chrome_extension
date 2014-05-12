72d6f8f6940ad46d8c983a86ddd4e5d6ab43699a
Fayland Lam <fayland@gmail.com>
2014-05-12 14:39:11 +0800

    init tieba

--- src/bg/background.js
+++ src/bg/background.js
@@ -45,10 +45,42 @@
                         success  : function(data) {
                             console.log(data);
                             // {"errcode":0,"msg":null,"data":[268,2]}
-                            if (data.errcode == 0) { // 1 is OK, 2 is already done
+                            if (data.errcode == 0) {
                                 var today = (new Date()).toDateString();
                                 localStorage.setItem('weibo_status', today);
                                 localStorage.setItem('weibo_points', data.data[1]);
+                            }
+                        },
+                        error: function(err) {
+                            console.log(err);
+                        }
+                    });
+                }
+            });
+        } else if (request.method == 'tieba_qiandao') {
+            console.log('tieba qiandao');
+            chrome.cookies.getAll({domain:"tieba.baidu.com"}, function (cookies){
+                var is_logined = 0;
+                for (var i in cookies) {
+                    if (cookies[i].name == 'BAIDUID') {
+                        is_logined = 1;
+                    }
+                }
+                console.log('is_logined:' + is_logined);
+                console.log(request);
+                is_logined = 0;
+                if (is_logined) {
+                    var url = 'http://tieba.baidu.com/sign/add';
+                    $.ajax({
+                        type     : "POST",
+                        url      : url,
+                        dataType : 'json',
+                        success  : function(data) {
+                            console.log(data);
+                            // {"no":0,"error":"","data":{"errno":0,"errmsg":"success","sign_version":2,"is_block":0,"finfo":{"forum_info":{"forum_id":8021166,"forum_name":"\u767e\u5ea6\u5f71\u68d2"},"current_rank_info":{"sign_count":1101}},"uinfo":{"user_id":889003992,"is_sign_in":1,"user_sign_rank":1101,"sign_time":1399875492,"cont_sign_num":1,"total_sign_num":2,"cout_total_sing_num":2,"hun_sign_num":1,"total_resign_num":0,"is_org_name":0}}}
+                            if (data.data.errno == 0) {
+                                var today = (new Date()).toDateString();
+                                localStorage.setItem('tieba_status', today);
                             }
                         },
                         error: function(err) {
